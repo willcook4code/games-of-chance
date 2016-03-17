8a4f3e91ae9d49368439e779dc058d268389c948
Will Cook <wellsee83@gmail.com>
2016-03-17 01:34:51 -0500

    Explorer Mode

--- scripts/main.js
+++ scripts/main.js
@@ -8,10 +8,47 @@ var aceOfSpades = new Card('Spades', 'Ace');
 console.log(aceOfSpades);
 
 // Six-sided Die Constructor
-function SixSidedDie (number, roll) {
-	this.number = number;
-	this.roll = roll;
-	this.rollDie = function(dieNumber) {
-		return(Math.floor(Math.random()*((6-1) + 1)) + 1)
+
+function SixSidedDie () {
+	this.faceNumber = Math.floor(Math.random()*6 + 1);
+	this.rollDie = function() {
+		var newNumber = Math.floor(Math.random()*6 + 1);
+		return newNumber;
+		//console.log(newNumber);
 	}
 }
+
+// var sumDie = {};
+
+// var firstDie = die1.rollDie(6);
+// var secondDie = die2.rollDie(6);
+
+function getProbabilities() {
+	var die1 = new SixSidedDie();
+	var	die2 = new SixSidedDie();
+	for (var i = 0; i < 1000; i++) {
+		
+		var possibleSums = [2,3,4,5,6,7,8,9,10,11,12];
+		var nthRole1= die1.rollDie();
+		var nthRole2= die2.rollDie();
+		var rolls = [nthRole1 + nthRole2];
+	
+		for (var j=0; j < rolls.length; j++) {
+		
+		var sum = possibleSums[j];
+			
+			if(sum in rolls) {
+				rolls[sum]++;
+			} else {
+				rolls[sum] = 1;
+			}
+			return rolls;
+		}
+	}
+	
+}
+
+console.log(getProbabilities());
+
+
+
